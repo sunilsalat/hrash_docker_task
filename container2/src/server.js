1e26a433835e29de4658ae538b401aa2fbd4f272
sunilsalat <sunilsalat@yahoo.com>
2024-05-30 22:37:56 +0530

    final changes

--- container2/src/server.js
+++ container2/src/server.js
@@ -1,91 +1,54 @@
-const { error } = require("console");
-const express = require("express");
-const fs = require("fs");
-const path = require("path");
-// const csv = require('csv-parser')
-const PORT = 8000;
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const csv = require('csv-parser')
+const PORT = 8000
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.get("/test", async (res) => {
-  return res.status(200).json({ msg: "Test passed" });
-});
+app.get('/test', async res => {
+  return res.status(200).json({ msg: 'Test passed' })
+})
 
-app.post("/result", async (req, res) => {
-  const payload = req.body;
-  const filePath = path.join(__dirname, `../../data/${payload.file}`);
-  let isCSV = true;
-  let totalAmount = 0;
+app.post('/result', async (req, res) => {
+  const payload = req.body
+  const filePath = path.join(__dirname, `../../data/${payload.file}`)
+  let isCSV = true
+  let totalAmount = 0
 
   try {
-    // const data = fs.readFileSync(filePath, "utf-8");
-
-    // if (!data) {
-    //   return res
-    //     .status(200)
-    //     .json({ file: payload.file, error: "Input file not in CSV format." });
-    // }
-
-    // const lines = data.split('\n')
-    // const headers = lines[0].split(',')
-    // const parsedData = lines.slice(1).map(line => {
-    //   const fields = line.split(',')
-    //   let obj = {}
-    //   headers.forEach((header, index) => {
-    //     obj[header.trim()] = fields[index].trim()
-    //   })
-    //   return obj
-    // })
-
-    // if (parsedData.length === 0) {
-    //   return res
-    //     .status(200)
-    //     .json({ file: payload.file, error: 'Input file not in CSV format.' })
-    // }
-
-    // for (let i = 0; i < parsedData.length; i++) {
-    //   console.log(parsedData[i])
-    //   let { product, amount } = parsedData[i]
-    //   console.log(product, amount)
-    //   if (product.toLowerCase() == payload.product.toLowerCase()) {
-    //     totalAmount += Number(amount)
-    //   }
-    // }
-
     fs.createReadStream(filePath)
       .pipe(csv())
-      .on("error", () => {
-        isCSV = false;
+      .on('error', () => {
+        isCSV = false
         return res
           .status(200)
-          .json({ file: payload.file, error: "Input file not in CSV format." });
+          .json({ file: payload.file, error: 'Input file not in CSV format.' })
       })
-      .on(data, (item) => {
-        console.log({ item });
-      })
-      .on("end", () => {
-        if (isCSV) {
-          console.log("csv found");
-
-          res.status(200).json({ file: `${payload.file}`, sum: totalAmount });
-        } else {
-          res.status(400).json({ error: "Invalid CSV file" });
+      .on('data', item => {
+        const { product, amount } = item
+        if (product.trim() === payload.product.trim()) {
+          totalAmount += Number(amount)
         }
-      });
+      })
+      .on('end', () => {
+        if (isCSV) {
+          console.log('csv found')
 
-    // return res.status(200).json({
-    //   file: `${payload.file}`,
-    //   sum: totalAmount,
-    // });
+          res.status(200).json({ file: `${payload.file}`, sum: totalAmount })
+        } else {
+          res.status(400).json({ error: 'Invalid CSV file' })
+        }
+      })
   } catch (error) {
     res.status(200).json({
       file: `${payload.file}`,
-      error: "Input file not in CSV format.",
-    });
+      error: 'Input file not in CSV format.'
+    })
   }
-});
+})
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
-});
+  console.log(`App running on port ${PORT}`)
+})
